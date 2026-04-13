5eb01b6427d0cd759e7de0e88148398a6032177f
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2026-04-13 20:05:46 +0200

    wip

--- MigraineTrackerUITests/MigraineTrackerUITestsLaunchTests.swift
+++ MigraineTrackerUITests/MigraineTrackerUITestsLaunchTests.swift
@@ -12,10 +12,11 @@ final class MigraineTrackerUITestsLaunchTests: XCTestCase {
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+        
     }
 
     @MainActor
-    func captureScreenshots() throws {
+    func testCaptureScreenshots() throws {
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
@@ -26,11 +27,11 @@ final class MigraineTrackerUITestsLaunchTests: XCTestCase {
         // https://developer.apple.com/documentation/xcuiautomation
         app.activate()
         snapshot("0Launch")
-        app/*@START_MENU_TOKEN@*/.buttons["Migräneanfall hinzufügen"]/*[[".buttons",".containing(.staticText, identifier: \"Migräneanfall hinzufügen\")",".containing(.image, identifier: \"plus.circle.fill\")",".groups.buttons[\"Migräneanfall hinzufügen\"]",".buttons[\"Migräneanfall hinzufügen\"]"],[[[-1,4],[-1,3],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.click()
+        app/*@START_MENU_TOKEN@*/.buttons["Migräneanfall hinzufügen"]/*[[".buttons",".containing(.staticText, identifier: \"Migräneanfall hinzufügen\")",".containing(.image, identifier: \"plus.circle.fill\")",".groups.buttons[\"Migräneanfall hinzufügen\"]",".buttons[\"Migräneanfall hinzufügen\"]"],[[[-1,4],[-1,3],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
         snapshot("1AddMigräne")
-        app.buttons["Abbrechen"].firstMatch.click()
+        app.buttons["Abbrechen"].firstMatch.tap()
 
-        app/*@START_MENU_TOKEN@*/.buttons["Einstellungen"]/*[[".groups[\"Einstellungen\"].buttons",".groups.buttons[\"Einstellungen\"]",".buttons[\"Einstellungen\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.click()
+        app/*@START_MENU_TOKEN@*/.buttons["Einstellungen"]/*[[".groups[\"Einstellungen\"].buttons",".groups.buttons[\"Einstellungen\"]",".buttons[\"Einstellungen\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
         snapshot("2Settings")
         app.terminate()
 
